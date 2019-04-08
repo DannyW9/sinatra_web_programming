99f756cd5d634202cf3c0013122f5600cdb32e10
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-08 20:34:40 +0100

    adds a home page with a link on the results view page

--- controller.rb
+++ controller.rb
@@ -3,6 +3,10 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('models/game.rb')
 
+get "/" do
+  erb (:home)
+end
+
 get "/:player1/:player2" do
   game = Game.new(params['player1'], params['player2'])
   @result = game.determine_winner
