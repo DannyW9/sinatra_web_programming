773317251107cc618dfc4694d5ecb261d51d8e1c
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-08 20:00:25 +0100

    changes logic to return both the player and the hand they won with

--- models/game.rb
+++ models/game.rb
@@ -7,12 +7,12 @@ end
 
 def determine_winner
   if @player1 == @player2
-    return "It's a draw!"
+    return "It's a draw! Both players picked #{@player1}!"
   elsif (@player1 == "rock" && @player2 == "scissors") ||
         (@player1 == "paper" && @player2 == "rock") ||
         (@player1 == "scissors" && @player2 == "paper")
-    return "Player 1 wins!"
-  else "Player 2 wins!"
+    return "Player 1 wins with #{@player1}!"
+  else "Player 2 wins with #{@player2}!"
   end
 end
 
